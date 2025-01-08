d82b0b02ca194d1c684cfc2edb7aa198d8fc1d17
Hugo Boss <hugoboss@Hugos-MacBook-Pro.local>
2025-01-08 00:24:21 -0400

    Update Raffle contract and remove Counter files

---
+++ src/Raffle.sol
@@ -0,0 +1,79 @@
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title A sample Raffle Contract
+ * @author H.Arenas
+ * @notice This contract is for creating a sample raffle
+ * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ */
+contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    // Data structure planning - Why use array?
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        //1. Makes migration easier
+        //2. Makes front-end "indexing" easier
+        emit RaffleEntered(msg.sender);
+    }
+
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        //get RandomNumber'VRF' from Chainlink
+    }
+
+    /** Getter Function */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
