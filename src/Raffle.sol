ff83be7ab146dba7f5218e076a70989efe55ded4
Icree8 <d1sptach@icloud.com>
2025-01-09 18:58:47 -0400

    random Module addes

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -106,7 +106,10 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] memory randomWords
-    ) internal virtual override {}
+    ) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.lenght;
+        address payable recentWinner = s_players[indexOfWinner];
+    }
 
     /**
      * Getter Function
